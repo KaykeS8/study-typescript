676dedd7c33bb47da8cc321963ad340a3a2dc591
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-19 20:15:10 -0300

    Arquivo da aula de criação de tipos

--- 7-criacao da tipos/index.ts
+++ 7-criacao da tipos/index.ts
@@ -15,7 +15,7 @@ const showProductName = <T extends objectTypes>(obj: T) => {
   return `O nome do produto é: ${obj.name}`
 }
 
-console.log(showProductName({ name: 'Dell', cor:"gray" }))
+console.log(showProductName({ name: 'Dell', cor: "gray" }))
 
 // 3 - generics com interface
 
@@ -46,4 +46,73 @@ const server = { hd: '10tb', ram: '160GB' }
 console.log(getSomeKey(server, 'ram'))
 console.log(getSomeKey(server, 'hd'))
 
-// 5 -
+// 5 - keyof type operator
+
+type charater = { name: string, age: number, hasDrivenLices: boolean }
+
+type C = keyof charater
+
+function showCharName(obj: charater, key: C): string {
+  return `Você accesou o dado:${obj[key]}`
+}
+
+const datesMyPerson: charater = {
+  name: 'kayke de souza simao oliveira',
+  age: 17,
+  hasDrivenLices: false
+}
+
+console.log(showCharName(datesMyPerson, 'hasDrivenLices'))
+
+// 6 - typeof type operator
+const userName: string = 'Kayke'
+const userName3: typeof userName = 'joão'
+
+// 7 - indexed Acess types
+
+type truck = { km: number, kg: number, description: string }
+type km = truck['km']
+
+const newTruck: truck = {
+  km: 10000,
+  kg: 8000,
+  description: 'Caminhão para pouca carga'
+}
+
+const car = {
+  kg: 2000,
+  km: 5000
+}
+
+function showDetailsOfTruck(km: km) {
+  console.log("Este veiculo possui Km: " + km)
+}
+
+showDetailsOfTruck(newTruck.km)
+showDetailsOfTruck(car.km)
+
+// 8 - conditional expression type
+
+interface A { }
+interface B extends A { }
+
+type myType = B extends A ? number : string
+
+const somaVar: myType = 15
+
+interface Teste {
+  showDetails(): string
+}
+
+type myTypeB = Teste extends { showDetails(): string } ? string : boolean
+
+const details: myTypeB = 'hello world, my name is kayke and i have 17 age, want to became developer software and in my free time i like to read a book,watch movie, run in street and speak with my friends'
+
+// 9 - Template literals type
+
+type testA = 'Front-end' | 'Back-end' | 'Full-stack'
+type CustomType = `some ${testA}`
+
+const message: CustomType = "some Front-end"
+
+console.log(message)
