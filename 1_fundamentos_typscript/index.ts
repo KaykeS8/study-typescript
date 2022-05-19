0ad680bbf70557eab68076bffdae13b7ab512699
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-19 14:27:13 -0300

    Arquivo da aula de fundamentos do typescript

---
+++ 1_fundamentos_typscript/index.ts
@@ -0,0 +1,52 @@
+// 1- numbers
+let x: number = 2004
+
+console.log(x)
+
+x = 17
+console.log(typeof x)
+
+const y: number = 3.234234
+
+console.log(typeof y, y.toPrecision(3))
+
+//2-string
+
+const firstName: string = 'kayke'
+
+console.log(firstName.toLocaleUpperCase())
+
+let fullname: string;
+
+const lastname: string = ' de souza simao oliveira'
+
+console.log(firstName.concat(lastname))
+
+//3 - boolean
+
+let a: boolean = true
+console.log(a, typeof a)
+
+a = true
+console.log(a)
+
+
+// 4 - inference e annotation
+
+const teste: string = 'teste'
+let dados = 'epa'
+
+// desafio - 1
+const somar = (num: number, num2: number) => {
+    console.log(num + num2)
+}
+
+somar(2, 3)
+
+// Desafio - 2
+
+const numero: number = 2
+const newString = numero.toString()
+const writeNumber: string = `O numero ${newString} está aqui`
+
+console.log(writeNumber)
