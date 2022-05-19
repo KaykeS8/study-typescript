eec3ba1f71baef0a348638d1d05f70bcae681e62
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-19 14:28:58 -0300

    Arquivo onde o código typescript sera compilado para javascript

---
+++ 1_fundamentos_typscript/index.js
@@ -0,0 +1,32 @@
+"use strict";
+// 1- numbers
+let x = 2004;
+console.log(x);
+x = 17;
+console.log(typeof x);
+const y = 3.234234;
+console.log(typeof y, y.toPrecision(3));
+//2-string
+const firstName = 'kayke';
+console.log(firstName.toLocaleUpperCase());
+let fullname;
+const lastname = ' de souza simao oliveira';
+console.log(firstName.concat(lastname));
+//3 - boolean
+let a = true;
+console.log(a, typeof a);
+a = true;
+console.log(a);
+// 4 - inference e annotation
+const teste = 'teste';
+let dados = 'epa';
+// desafio - 1
+const somar = (num, num2) => {
+    console.log(num + num2);
+};
+somar(2, 3);
+// Desafio - 2
+const numero = 2;
+const newString = numero.toString();
+const writeNumber = `O numero ${newString} está aqui`;
+console.log(writeNumber);
