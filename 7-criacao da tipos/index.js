523e4602cb168dc726fef8014793a4b62e3f108a
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-19 20:15:50 -0300

    Arquivo com o código typescript compilado

--- 7-criacao da tipos/index.js
+++ 7-criacao da tipos/index.js
@@ -20,4 +20,33 @@ function getSomeKey(obj, key) {
 const server = { hd: '10tb', ram: '160GB' };
 console.log(getSomeKey(server, 'ram'));
 console.log(getSomeKey(server, 'hd'));
-// 5 -
+function showCharName(obj, key) {
+    return `Você accesou o dado:${obj[key]}`;
+}
+const datesMyPerson = {
+    name: 'kayke de souza simao oliveira',
+    age: 17,
+    hasDrivenLices: false
+};
+console.log(showCharName(datesMyPerson, 'hasDrivenLices'));
+// 6 - typeof type operator
+const userName = 'Kayke';
+const userName3 = 'joão';
+const newTruck = {
+    km: 10000,
+    kg: 8000,
+    description: 'Caminhão para pouca carga'
+};
+const car = {
+    kg: 2000,
+    km: 5000
+};
+function showDetailsOfTruck(km) {
+    console.log("Este veiculo possui Km: " + km);
+}
+showDetailsOfTruck(newTruck.km);
+showDetailsOfTruck(car.km);
+const somaVar = 15;
+const details = 'hello world, my name is kayke and i have 17 age, want to became developer software and in my free time i like to read a book,watch movie, run in street and speak with my friends';
+const message = "some Front-end";
+console.log(message);
