d6a0ff7539cb10c4e813a93435ad979c5f6cd639
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-26 18:00:49 -0300

    Arquivo com a compilação do código typescript

---
+++ 9 - modulos/great.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Greeting = void 0;
+function Greeting() {
+    console.log('Esto aqui carai');
+}
+exports.Greeting = Greeting;
