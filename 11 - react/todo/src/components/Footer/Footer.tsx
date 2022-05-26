9abb6021fa1eb2eb0352c750036f92950c7f3fdf
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-26 20:51:44 -0300

    Componente Footer

---
+++ 11 - react/todo/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.css'
+
+export const Footer = () => {
+  return (
+    <footer className='footer'>
+      <p>
+        <span>React + TS</span>
+        <a href="https://www.github.com/kaykeS8" target="_blank">Kayke Simão</a>
+      </p>
+    </footer>
+  )
+}
