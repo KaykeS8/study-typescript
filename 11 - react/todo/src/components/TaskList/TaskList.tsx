8167a4df737c55b741399c29173bb7b22b9c8462
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-29 15:23:32 -0300

    Componente TaskList para a criação da lista de tasks

--- 11 - react/todo/src/components/TaskList/TaskList.tsx
+++ 11 - react/todo/src/components/TaskList/TaskList.tsx
@@ -3,23 +3,25 @@ import './taskList.css'
 
 
 type Props = {
-  taskList: ITask[]
+  taskList: ITask[];
+  handleDelete(id: number): void;
+  handleEdit: (task:ITask) => void
 }
 
-export const TaskList = ({ taskList }: Props) => {
+export const TaskList = ({ taskList, handleDelete, handleEdit }: Props) => {
   return (
     <>
       {taskList.length > 0 ? (
-        taskList.map(item => (
-          <div key={item.id} className="task">
-              <div className='details'>
-                <h4>{item.title}</h4>
-                <p>Dificuldade: {item.difficulty}</p>
-              </div>
-              <div className='actions'>
-                <i className='bi bi-pencil'></i>
-                <i className='bi bi-trash'></i>
-              </div>
+        taskList.map(task => (
+          <div key={task.id} className="task">
+            <div className='details'>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className='actions'>
+              <i className='bi bi-pencil' onClick={() => handleEdit(task)}></i>
+              <i className='bi bi-trash' onClick={() => handleDelete(task.id)}></i>
+            </div>
           </div>
         ))
       ) : (
