58dd71f0fa4ef1886d0000776bd45c73428ac5f0
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-06-24 17:06:10 -0300

    Arquivo onde o código typescript esta sendo compilado para javascript

--- 5_Funções_com_typescript/index.js
+++ 5_Funções_com_typescript/index.js
@@ -1,7 +1,7 @@
 "use strict";
 //  1 - void
 function withoutReturn() {
-    console.log('Esta função não tem retorno');
+    console.log("Esta função não tem retorno");
 }
 //  2 - callback com argumento
 const greeting = (name) => {
@@ -12,8 +12,8 @@ const showTheUsers = (func, user) => {
     const greet = func(user);
     console.log(greet);
 };
-showTheUsers(greeting, 'layle');
-showTheUsers(greeting, 'Julia');
+showTheUsers(greeting, "layle");
+showTheUsers(greeting, "Julia");
 //  3 - generic functions
 const firstElement = (arr) => {
     return arr[0];
@@ -21,10 +21,14 @@ const firstElement = (arr) => {
 function margeObjects(obj1, obj2) {
     return Object.assign(Object.assign({}, obj1), obj2);
 }
-const newObject = margeObjects({ nome: 'kayke', age: 17 }, { date: 'Node.js', salary: 95694 });
+const newObject = margeObjects({ nome: "kayke", age: 17 }, { date: "Node.js", salary: 95694 });
 console.log(newObject);
 const { nome, date } = newObject;
 console.log(nome);
+function indetidade(arg, arg2) {
+    return arg;
+}
+console.log(indetidade("Kalita", "hello"));
 //  4 - constraints
 function biggestNumber(a, b) {
     let bigest;
@@ -37,13 +41,13 @@ function biggestNumber(a, b) {
     return bigest;
 }
 console.log(biggestNumber(12, 32));
-console.log(biggestNumber('123', '23'));
+console.log(biggestNumber("123", "23"));
 // 5 - especificar tipo de argumento
 const mergeArray = (arr1, arr2) => {
     return arr1.concat(arr2);
 };
 console.log(mergeArray([1, 2, 3], [4, 5, 6]));
-console.log(mergeArray([2, 6, 12, 44], ['pares', 'impars']));
+console.log(mergeArray([2, 6, 12, 44], ["pares", "impars"]));
 //  6 - Parametros opcionais
 function modernGreeting(name, greet) {
     if (greet) {
@@ -51,14 +55,14 @@ function modernGreeting(name, greet) {
     }
     return `Olá ${name}`;
 }
-console.log(modernGreeting('Julia'));
-console.log(modernGreeting('kayke', 'desenvolvedor'));
+console.log(modernGreeting("Julia"));
+console.log(modernGreeting("kayke", "desenvolvedor"));
 //  7 - parâmetro default
 const sumTwoNumbers = (numOne, numTwo = 5) => {
     return numOne / numTwo;
 };
 console.log(sumTwoNumbers(200));
-//  8 - tipo 
+//  8 - tipo
 function doSomething(x) {
     if (Array.isArray(x)) {
         console.log(x[0]);
@@ -81,7 +85,7 @@ function showProductsDetails({ name, descricao }) {
     return `O nome do produto é ${name} e sua descrição é essa ${descricao}`;
 }
 const objPhone = {
-    name: 'Iphone',
-    descricao: 'Um produto muito reverienciado no mercado da tecnologia pela sua alta qualidade'
+    name: "Iphone",
+    descricao: "Um produto muito reverienciado no mercado da tecnologia pela sua alta qualidade",
 };
 console.log(showProductsDetails(objPhone));
